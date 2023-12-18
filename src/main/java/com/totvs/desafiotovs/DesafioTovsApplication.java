db69ca71943df152bfd67ca15e1cd9fe5633b57f
João Augusto Moreira Ananias <joao.ananias@aluno.ueg.br>
2023-12-18 11:25:49 -0300

    Initial Commit

---
+++ src/main/java/com/totvs/desafiotovs/DesafioTovsApplication.java
@@ -0,0 +1,13 @@
+package com.totvs.desafiotovs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioTovsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioTovsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/totvs/desafiotovs/DesafioTovsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.totvs.desafiotovs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTovsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
